caaaa623f0fd3f016fe1528589c2468b7be2e0cd
Wilmela <chukumelawilson@gmail.com>
2022-08-29 13:34:00 +0100

    adjusted margin-top for hero image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,12 @@
 import "./Header.css";
 import { motion } from "framer-motion";
 
+import BG from '../../assets/weather-header.png'
+
 const Header = () => {
   return (
     <div className="app__header">
-      <motion.h1
-       whileInView={{ x: [-100, 0] }} 
-       transition={{ duration: 0.5 }}
-       >
+      <motion.h1 whileInView={{ x: [-100, 0] }} transition={{ duration: 0.5 }}>
         Welcome!
       </motion.h1>
     </div>
